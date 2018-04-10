d5ddd45a1d73834e60dfc101704fc2744815d5fb
Coooolbreeze <392113643@qq.com>
2018-04-10 20:37:37 +0800

    修改登录页

--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -2,31 +2,30 @@ var base = window.base;
 base.getLocalStorage('token') && (window.location.href = 'home.html');
 
 layui.use(['layer'], function () {
-    var data = {
-        username: '',
-        password: ''
-    }
-
-    $('#login').on('click', function () {
-        data.username = $('#username').val();
-        data.password = $('#password').val();
-
-        login(function (data) {
-            base.setLocalStorage('token', data.access_token, data.expire_in * 1000);
-            base.setLocalStorage('refresh_token', data.refresh_token);
-            window.location.href = 'home.html';
-        });
-    });
-
-    function login(callback) {
+    $(document).on('click', '#login', function () {
+        var $userName = $('#username'),
+            $pwd = $('#password');
+        if (!$userName.val()) {
+            $userName.next().show().find('div').text('请输入用户名');
+            return;
+        }
+        if (!$pwd.val()) {
+            $pwd.next().show().find('div').text('请输入密码');
+            return;
+        }
         var params = {
             url: '/login',
             type: 'POST',
-            data: data,
+            data: { username: $userName.val(), password: $pwd.val() },
             success: function (res) {
-                callback && callback(res.data);
+                base.setLocalStorage('token', res.data.access_token, res.data.expire_in * 1000);
+                base.setLocalStorage('refresh_token', res.data.refresh_token);
+                window.location.href = 'home.html';
+            },
+            fail: function (e) {
+                $('.error-tips').text('帐号或密码错误').show().delay(2000).hide(0);
             }
-        }
+        };
         base.getData(params);
-    }
+    });
 });
